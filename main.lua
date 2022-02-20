e4262152a45918f9da913f3525cc489422a6fa7c
SawSoftware <bradster1999@outlook.com>
2022-02-20 08:23:22 -0500

    Added more widget GUI commands -- OPEN SOURCE

--- main.lua
+++ main.lua
@@ -1,21 +1,20 @@
 function love.load()
 require("modules.gui")
+newWidget("job_setup",250,250,100,100)
+
 end
 
 function love.draw()
-  if not widgets == nil then
-    drawWidgets()
-  else
-    -- DEBUG WHEN NO WIDGETS TO DRAW
-    love.graphics.print("NO WIDGETS TO DRAW",0,0)
-  end
-  if not buttons == nil then
-    drawButtons()
-  else
-    love.graphics.print("NO BUTTONS TO DRAW",0,15)
-  end
+drawWidgets()
 end
 
 function love.update(dt)
-
+  ww,wh = love.graphics.getDimensions()
+  changeWidgetPS("job_setup",(ww/2),wh/2)
+  if love.keyboard.isDown("p") then
+    hideWidget("2")
+  end
+  if love.keyboard.isDown("k") then
+    showWidget("2")
+  end
 end
--- modules/gui.lua
+++ modules/gui.lua
@@ -1,13 +1,12 @@
 -- Start point
 widgets = {}
--- Modules with in a widget
-buttons = {}
 
 
 
 --- Start Widget
 function newWidget(id,x,y,w,h)
   table.insert(widgets,{id = id,x = x,y = y,w = w,h = h,hidden = false})
+  print("Made widget with the ID of: "..id)
 end
 
 function drawWidgets()
@@ -16,16 +15,27 @@ function drawWidgets()
       love.graphics.rectangle("fill", v.x, v.y, v.w, v.h)
     end
   end
+end
 
+function changeWidgetPS(id,x,y,w,h)
+  for k,v in ipairs(widgets) do
+    if id == v.id then
+      v.x = x
+      v.y = y
+    if w == nil and h == nil then
+      -- Do not change width and height
+    else
+      v.w = w
+      v.h = h
+    end
+    end
+  end
 end
 
 function showWidget(id)
   for k,v in ipairs(widgets) do
     if id == v.id then
       v.hidden = false
-    else
-      v.hidden = false
-      print("ID: "..id..", does not exist!")
     end
   end
 end
@@ -35,24 +45,6 @@ function hideWidget(id)
   for k,v in ipairs(widgets) do
     if id == v.id then
       v.hidden = true
-    else
-      v.hidden = false
-      print("ID: "..id..", does not exist!")
-    end
-  end
-end
--- END WIDGET
-
--- START BUTTONS
-
-function newButton(id,text,x,y,w,h,fn,pw_id)
-  table.insert(buttons,{id = id,text = text, x = x,y = y, w = w, h = h, fn = fn,pw_id = pw_id,hidden = false})
-end
-
-function drawButtons()
-  for k,v in ipairs(buttons) do
-    if v.hidden == false then
-      love.graphics.rectangle("fill", v.x, v.y, v.w, v.h)
     end
   end
 end
